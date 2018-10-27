e239b7a56783db192e9869ef40b461d1409c79b1
Jean navea <jeans.navea@digital-cupcake.com>
2018-10-27 14:57:27 -0400

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/jeansnavea'));
-
+app.use(express.static(__dirname + '/dist/jeansnavea'));
 app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname + '/dist/jeansnavea/index.html'));
 
-res.sendFile(path.join(__dirname,'/dist/jeansnavea/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
